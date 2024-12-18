78ba6187ea62f53b50bf189b20e74fe4425da279
Samarbaev Ruslan <samarbaev36@gmail.com>
2024-12-18 13:51:12 +0300

    9_counting_s_t_n

---
+++ chapter_1/src/9_counting_s_t_n.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define TAB '\t'
+#define END_OF_ROW '\n'
+#define SPACE ' '
+
+int main() {
+    
+    int countTab = 0;
+    int countEndOfRow = 0;
+    int countSpace = 0;
+
+    int input;
+
+    while ((input = getchar()) != EOF) {
+        if (input == TAB) ++countTab;
+        if (input == END_OF_ROW) ++countEndOfRow;
+        if (input == SPACE) ++countSpace;
+    }
+
+    printf("count TAB \\t = %d\n", countTab);
+    printf("count EOR \\n = %d\n", countEndOfRow);
+    printf("count SPACE \\' ' = %d\n", countSpace);
+
+}
